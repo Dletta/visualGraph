0cb14ada3a95efb9dacee16350cd717b23336149
Dletta <jcduschletta@gmail.com>
2018-09-06 20:37:21 -0700

    Abstraction for Nodes, Edges amongst other items

---
+++ abstraction.js
@@ -0,0 +1,11 @@
+function tuple (node, verb, object){
+  var nodes = gun.get('nodes');
+  var edges = gun.get('edges');
+  var node = nodes.set(node);
+  var verb = edges.set(verb);
+  var object = nodes.set(object);
+  node.get('out').set(verb);
+  verb.get('source').put(node);
+  verb.get('target').put(object);
+  object.get('in').set(verb);
+}
