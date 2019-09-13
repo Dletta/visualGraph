49f451a0e2052c172d91efcd0c471c4b31f43f8b
Dletta <jcduschletta@gmail.com>
2019-09-12 21:53:48 -0700

    when we have to keep things protected but collaborative

--- abstraction.js
+++ abstraction.js
@@ -1,3 +1,28 @@
+/*
+Allowing 2 users to collaborate on the same path.
+This makes it write only for each user, but in a UI using below function you
+can now see whatever was added latest collaboration
+*/
+
+async function getLatest (path,pubkeyOther) {
+  var path = path;
+  var refMe = gun.user();
+  var refBob = gun.user(pubkeyOther);
+  console.log('me', refMe);
+  console.log('bob', refBob);
+  while(path.length>0){
+    var step = path.shift();
+    refMe = refMe.get(step);
+    refBob = refBob.get(step);
+  }
+  var me = await refMe.promOnce();
+  console.log('me', me.data);
+  var bob = await refBob.promOnce();
+  console.log('bob', bob.data);
+  return Gun.state.node(me.data, bob.data);
+}
+
+
 /* Abstraction Layer to GunDB
 * Functions to abstract the creation of a schema
 */
