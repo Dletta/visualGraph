3166173d5903e5376df550a02218c5b5fb11303a
Dletta <jcduschletta@gmail.com>
2019-09-30 22:23:50 -0700

    updated

--- visualGraph.js
+++ visualGraph.js
@@ -1,3 +1,5 @@
+/* SECTION: D3 functionality */
+
 function dragstarted(d) {
   if (!d3.event.active) simulation.alphaTarget(0.3).restart();
   d.fx = d.x;
@@ -16,14 +18,22 @@ function dragended(d) {
 }
 
 function ticked() {
-  link.attr("x1", function(d) { return d.source.x; })
+  link
+      .attr("x1", function(d) { return d.source.x; })
       .attr("y1", function(d) { return d.source.y; })
       .attr("x2", function(d) { return d.target.x; })
       .attr("y2", function(d) { return d.target.y; });
 
-  node.attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
-      })
+  node
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      .attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+          });
+
+  label
+      .attr("x", function(d) { return d.x + 5; })
+      .attr("y", function(d) { return d.y + 3; })
 }
 
 function makeLabel(data) {
@@ -56,6 +66,44 @@ node.attr("transform", d3.event.transform);
 label.attr("transform", d3.event.transform);
 }
 
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var simulation = d3.forceSimulation();
+simulation.force("charge", d3.forceManyBody().strength(-600));
+simulation.force("link", d3.forceLink().id(function(d) { return d.id; }));
+simulation.force("center", d3.forceCenter(width/2,height/2));
+
+function update () {
+console.log('updated');
+svg.selectAll('*').remove();
+link = svg.append("g").attr("class","links").selectAll("line").data(graph.edges).enter().append("line");
+node = svg.append("g").attr("class", "nodes").selectAll("circles").data(graph.nodes).enter()
+              .append("circle").attr("r", 2.5).attr('fill', 'red').call(d3.drag().on("start",dragstarted).on("drag", dragged).on("end",dragended))
+              .on("click", detail);
+node.append("title").text((d)=>{return d.id.toUpperCase();});
+label = svg.selectAll('text').data(graph.nodes).enter().append("text").text((d)=>{return d.label.toUpperCase()}).attr('x', (d)=>{return d.x});
+simulation.nodes(graph.nodes).on("tick", ticked);
+simulation.force("link").links(graph.edges);
+simulation.restart();
+
+  function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+    label
+        .attr("x", function(d) { return d.x + 5; })
+        .attr("y", function(d) { return d.y + 3; })
+  }
+};
+
+/* SECTION: Graph Inspector */
+
 function detail(ev) {
   console.log(ev);
   gun.get(ev.id).once((data, key) => {
@@ -83,37 +131,129 @@ function detail(ev) {
   });
 }
 
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+/* SECTION: DFS functionality */
 
-var simulation = d3.forceSimulation();
-simulation.force("charge", d3.forceManyBody().strength(-600));
-simulation.force("link", d3.forceLink().id(function(d) { return d.id; }));
-simulation.force("center", d3.forceCenter(width/2,height/2));
+var DFS = (function () {
 
-function update () {
-console.log('updated');
-svg.selectAll('*').remove();
-link = svg.append("g").attr("class","links").selectAll("line").data(graph.edges).enter().append("line");
-node = svg.append("g").attr("class", "nodes").selectAll("circles").data(graph.nodes).enter()
-              .append("circle").attr("r", 2.5).attr('fill', 'red').call(d3.drag().on("start",dragstarted).on("drag", dragged).on("end",dragended))
-              .on("click", detail);
-node.append("title").text((d)=>{return d.id.toUpperCase();});
-label = svg.selectAll('text').data(graph.nodes).enter().append("text").text((d)=>{return d.label.toUpperCase()}).attr('x', (d)=>{return d.x});
-simulation.nodes(graph.nodes).on("tick", ticked);
-simulation.force("link").links(graph.edges);
-simulation.restart();
-function ticked() {
-link
-    .attr("x1", function(d) { return d.source.x; })
-    .attr("y1", function(d) { return d.source.y; })
-    .attr("x2", function(d) { return d.target.x; })
-    .attr("y2", function(d) { return d.target.y; });
+  var stack;
+  var nodes;
+  var edges;
+  var start;
+  var u;
+  var label;
+  var opt = false;
+  var stop = false;
+  var limit = 300;
 
-node
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; });
-label
-    .attr("x", function(d) { return d.x + 5; })
-    .attr("y", function(d) { return d.y + 3; })
-}
-};
+  var util = {};
+
+  util.makeNodes = function (map){
+    var array = Array.from(map);
+    var nodes = [];
+    var i =0;
+    var l = array.length;
+    for(;i<l;i++){
+      nodes.push(array[i][1])
+    }
+    return nodes;
+  };
+
+  util.makeEdges = function (map) {
+    var array = Array.from(map);
+    var edges = [];
+    var i =0;
+    var l = array.length;
+    for(;i<l;i++){
+      edges.push(array[i][1])
+    }
+    return edges;
+  };
+
+  var dfs = {};
+
+  dfs.explore = async function(soul, lbl){
+    console.log('Starting with:',soul);
+    if(lbl){opt = true;} else { opt = false;}
+    label = lbl;
+    start = soul;
+    stack = [];
+    nodes = new Map();
+    edges = new Map();
+    var node = await gun.get(soul).promOnce();
+    dfs.node(node);
+  };
+
+  dfs.node = function (node) {
+    if(!node.data){console.log('no data:',node.key, node.data); dfs.back();return;}
+    var soul = Gun.node.soul(node)/*node['_']['#'] || node['#']*/;
+    if(soul == start){
+      stack.push(soul);
+    }
+    u = node;
+    if(!opt){
+      nodes.set(soul, {id:soul,label:key})
+    } else {
+      nodes.set(soul, {id:soul,label:node.data.label})
+    }
+
+    dfs.edge(u, edges);
+  };
+
+  dfs.edge = function (node, edges) {
+    if(stop){console.log('stopped');return;}
+    var temp;
+    var soul = Gun.node.soul(node)/*node['_']['#'] || node['#']*/;
+    var tLabel = soul;
+    var arr = Object.keys(node);
+    for(var prop in arr){
+      if(prop !== "_"){ //ignore metadata
+        if(prop == label) { tLabel = node[prop] }
+        if(typeof(node[prop]) == 'object' && node[prop] != null){
+          if(!edges.has(soul+node[prop]['#'])){
+            var temp = node[prop];
+            break;
+          }
+        }
+      }
+    }
+    if(temp){
+      dfs.next(temp, soul, temp['#'], tLabel);
+    } else {
+      if(start == soul) {stack.pop()}
+      dfs.back();
+    }
+  };
+
+  dfs.next = async function (next, edgeS, edgeT, tLabel) {
+    console.log('next', next);
+    var v = next;
+    var soul = v['#'];
+    nodes.set(soul, {id:soul,label:tLabel})
+    edges.set(edgeS+edgeT, {source:edgeS,target:edgeT})
+    stack.push(soul)
+    dfs.render();
+    u = v;
+    if(nodes.size >= limit){console.log('Reached limit');dfs.render();return;}
+    var node = await gun.get(soul).promOnce();
+    dfs.node(node);
+  };
+
+  dfs.back = function () {
+    if(!(stack.length == 0)){
+      soul = stack.pop();
+      gun.get(soul).once(dfs.node)
+    } else {
+      dfs.render();
+    }
+  };
+
+  dfs.render = function () {
+    console.log('Rendering');
+    graph.nodes = util.makeNodes(nodes);
+    graph.edges = util.makeEdges(edges);
+    update();
+  };
+
+  return dfs;
+
+})()
